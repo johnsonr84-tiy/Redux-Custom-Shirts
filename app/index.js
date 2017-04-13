96b2fc54f03cab54f2179acab5b5c51a66a1764d
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-04-13 11:47:42 -0600

    chore (main) Add lots of stuffies to js files and remove css

--- app/index.js
+++ app/index.js
@@ -1,1 +1,31 @@
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const Redux = require('redux')
+Redux.createStore
+
 console.log('Hello Shirts')
+
+const store = createStore( reducer, typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const newShirtDiv = document.querySelector('#createShirt')
+newShirtDiv.querySelector('#createShirtButton').onclick = () => {
+  store.dispatch({
+    type: 'SHIRT@ADD',
+    color: newShirtDiv.querySelector('#color').value,
+    size: newShirtDiv.querySelector('#size').value,
+    motto: newShirtDiv.querySelector('#motto').value
+  })
+}
+
+document.querySelector('#removeButton').onclick = () => store.dispatch({
+  type: 'SHIRT@REMOVE',
+  id: parseInt(document.querySelector('#id').value)
+})
+
+store.subscribe( () => {
+console.log("store.getState():", store.getState())
+})
+
+// const removeShirtDiv = document.querySelector('#removeShirt')
--- app/reducer.js
+++ app/reducer.js
@@ -1,11 +1,19 @@
-module.exports = function reducer(state, action) {
-  return [
-    ...state,
-    {
-    id: state[state.length -1] ?
-        state[state.length -1].id + 1 : 1,
-    color: action.color,
-    size: action.size,
-    motto: action.motto
-  }]
+module.exports = function reducer(state = [], action = {}) {
+  switch (action.type) {
+    case 'SHIRT@ADD':
+      return [
+        ...state,
+        {
+        id: state[state.length -1] ?
+            state[state.length -1].id + 1 : 1,
+        color: action.color,
+        size: action.size,
+        motto: action.motto
+    }]
+    break
+    case 'SHIRT@REMOVE':
+    return state.filter( shirt => shirt.id !== action.id)
+    default:
+    return state
+  }
 }
